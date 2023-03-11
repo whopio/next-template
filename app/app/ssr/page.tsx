5f3f502dac56e759c8704d847eedd4b68a3f4a97
Jack LaFond <jack.lafond@icloud.com>
2023-03-11 14:39:56 -0500

    apply styles on ssr page

--- app/app/ssr/page.tsx
+++ app/app/ssr/page.tsx
@@ -1,77 +1,123 @@
 import getSdk from "@/lib/get-user-sdk/app";
 import { NextAppPage } from "@/types/app-dir";
+import { Inter } from "@next/font/google";
+import { signIn, signOut } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../../../styles/Home.module.css";
 import Button from "./buttons";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const Page: NextAppPage = async () => {
   const { sdk, user } = await getSdk();
   return (
-    <>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <a
+          href="/"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>&lt;-</span> Go back
+        </a>
+        <p>
+          Edit this page inside of{" "}
+          <code className={styles.code}>pages/ssr/index.tsx</code>
+        </p>
+      </div>
+
+      <div className={styles.center}>
+        <div className={styles.otherbox}>
+          <h1
+            className={inter.className}
+            style={{
+              paddingLeft: "5px",
+            }}
+          >
             Welcome to{" "}
-            <a
+            <Link
               target="_blank"
               rel="noopener noreferrer"
               href="https://dash.whop.com"
             >
-              Whop!
-            </a>
+              Whop{user ? `, ${user.name}!` : `!`}
+            </Link>
           </h1>
-          <p className={styles.description}>
+          <p
+            className={inter.className}
+            style={{
+              paddingTop: "20px",
+              paddingLeft: "5px",
+            }}
+          >
             This page could be your homepage before a user accesses your
             application, and this state is meant to represent a user who is{" "}
-            <b>{sdk ? "logged in" : "logged out"}. </b>{" "}
-            {sdk && <Button loggedIn={!!sdk}> Click here to logout</Button>}
-            <br></br>
-            <br></br>You can edit this page in{" "}
-            <code className={styles.code}>pages/app/page.tsx</code>
+            <b>{user ? "logged in" : "logged out"}. </b> <br></br>
           </p>
-
-          {!sdk ? (
-            <div className={styles.grid}>
-              <a href="#" className={styles.card}>
-                <Button>
-                  <h2>Login &rarr;</h2>
-                  <p>Proceed to sign in with Whop</p>
-                </Button>
-              </a>
-            </div>
-          ) : (
-            <>
-              <p style={{ textAlign: "center" }}>
-                Logged in user object: {JSON.stringify(user, null, 2)}
-              </p>
-              <div className={styles.grid}>
-                <a href="/app/ssr/product-gated" className={styles.card}>
-                  <h2>Access Application &rarr;</h2>
-                  <p>This is an SSR gating of your application.</p>
-                </a>
-                <a href="/app/ssg/product-gated" className={styles.card}>
-                  <h2>Access Application &rarr;</h2>
-                  <p>This is an SSG gating of your application.</p>
-                </a>
-              </div>
-            </>
-          )}
-        </main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://dash.whop.com"
-            target="_blank"
-            rel="noopener noreferrer"
+          <div
+            style={{
+              paddingTop: "20px",
+            }}
           >
-            Powered by{" "}
-            <span className={styles.logo}>
-              <Image src="/logo.svg" alt="Whop Logo" width={72} height={16} />
-            </span>
-          </a>
-        </footer>
+            {sdk ? (
+              <p style={{ textAlign: "center" }} className={styles.card}>
+                Logged in user object:{" "}
+                <code>{JSON.stringify(user, null, "\t")}</code>
+              </p>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
       </div>
-    </>
+      <div>
+        {!sdk ? (
+          <div
+            className={styles.grid}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button>
+              <h2 className={inter.className}>Login &rarr;</h2>
+              <p className={inter.className}>Proceed to sign in with Whop</p>
+            </Button>
+          </div>
+        ) : (
+          <div
+            className={styles.grid}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button loggedIn={!!sdk}>
+              <h2 className={inter.className}>&larr; Logout </h2>
+              <p className={inter.className}>
+                This is an SSR gating of your application.
+              </p>
+            </Button>
+            <Link href="/ssr/product-gated" className={styles.card}>
+              <h2 className={inter.className}>Application (SSR) &rarr;</h2>
+              <p className={inter.className}>
+                This is an SSR gating of your application.
+              </p>
+            </Link>
+            <Link href="/ssg/product-gated" className={styles.card}>
+              <h2 className={inter.className}>Application (SSG)&rarr;</h2>
+              <p className={inter.className}>
+                This is an SSG gating of your application.
+              </p>
+            </Link>
+          </div>
+        )}
+      </div>
+    </main>
   );
 };
 
