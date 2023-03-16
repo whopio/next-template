a964ae97cf26612eb805138d2c8042034410e62f
Jack LaFond <jack.lafond@icloud.com>
2023-03-16 15:16:44 -0400

    Update design of template + new SDK version (#5)
    This PR includes the following changes
    - Bump SDK version to 0.0.4, the latest available
    - Redesign the template to use Vercel's newest Next.js 13 design
    - fixes INFRA-60

--- app/app/ssg/product-gated/page.tsx
+++ app/app/ssg/product-gated/page.tsx
@@ -1,4 +1,16 @@
 import { NextAppPage } from "@/types/app-dir";
+import Head from "next/head";
+import styles from "../../../../styles/Home.module.css";
+import { Inter } from "next/font/google";
+
+export const metadata = {
+  title: "Whop Powered Application (SSG)",
+  icons: {
+    icon: [{ url: "/logo.svg" }, new URL("/logo.svg", "https://whop.com")],
+  },
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 /**
  * this page is protected by the middleware, so if
@@ -6,7 +18,40 @@ import { NextAppPage } from "@/types/app-dir";
  * be statically served
  */
 const Page: NextAppPage = () => {
-  return <>Product owned!</>;
+  return (
+    <>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <div className={styles.description}>
+            <a
+              href="/app/ssr"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>&lt;-</span> Go back
+            </a>
+            <p>
+              Edit this page inside of{" "}
+              <code className={styles.code}>
+                app/app/ssg/product-gated/page.tsx
+              </code>
+            </p>
+          </div>
+
+          <div className={styles.center}>
+            <div className={styles.otherbox}>
+              <h1 className={inter.className}>Access Granted 🚀 (SSG)</h1>
+              <p className={inter.className}>
+                This page is protected by the middleware, so if no SSG bailouts
+                are used here, the page will be statically served.
+              </p>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Page;
--- app/app/ssr/product-gated/PurchaseLink.tsx
+++ app/app/ssr/product-gated/PurchaseLink.tsx
@@ -3,6 +3,7 @@
 import { usePurchaseLink } from "@/lib/get-purchase-link";
 import Link from "next/link";
 import { FunctionComponent, PropsWithChildren } from "react";
+import styles from "../../../../styles/Home.module.css";
 
 type PurchaseLinkProps = {
   plan: string;
@@ -18,7 +19,7 @@ const PurchaseLink: FunctionComponent<PropsWithChildren<PurchaseLinkProps>> = ({
 }) => {
   const link = usePurchaseLink(plan, redirect);
   return (
-    <Link className={className} href={link}>
+    <Link className={styles.card} href={link}>
       {children}
     </Link>
   );
